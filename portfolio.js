f8b264531435f5d4f726d6679be31245b7c195ca
kfiddle <kenjfiddle@gmail.com>
2020-10-17 21:31:02 -0400

    building out javascript

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,8 @@
 const codeScreenshots = document.querySelectorAll('.codeScreenShots');
 const ambition = document.getElementById('ambitionButton');
-
+const projectSection = document.querySelector('.projects');
+const aboutMeSection = document.getElementById('aboutMe');
+const finalStatement = document.createElement('h3');
 
 for (let i = 0; i < codeScreenshots.length; i++) {
     codeScreenshots[i].addEventListener('mouseover', grow);
@@ -15,15 +17,50 @@ for (let i = 0; i < codeScreenshots.length; i++) {
     }
  };
 
- ambition.addEventListener('click', bloom);
+ ambition.addEventListener('click', ambitionButtonFun);
+ 
+function vanish(e) {
+    ambition.classList.remove('bloom');
+    ambition.style.fontWeight = "light";
+    ambition.innerText = "But until then, here's what I've done so far...";
+    ambition.style.fontSize = "1em";
+    ambition.style.color = 'red';
+    setTimeout(displayProjects, 2100);
+}
 
- function bloom(e) {
-     ambition.classList.add('bloom');
-     ambition.style.fontWeight = "bold";
-     ambition.innerText = "Become a JavaScript Showoff!";
-     ambition.style.color = "red";
-     
+ function ambitionButtonFun(e) {
+    ambition.classList.add('bloom');
+    ambition.innerText = "to become a JavaScript Showoff!";
+    setTimeout(finalStatementLoop, 1300);
+
+    function finalStatementLoop() {
+        for (let i = 1; i < 4; i++){
+            displayFunctions = [but, until, then];
+            setTimeout(displayFunctions[i-1], (300*i));
+        }
+    }
  }
-    
 
+var displayProjects = function() {
+    projectSection.classList.add('projectsDisplay');
+}
 
+function but() {
+finalStatement.innerText = "But";
+aboutMeSection.appendChild(finalStatement);
+
+}
+
+function until() {
+    finalStatement.innerText = "But until";
+    aboutMeSection.appendChild(finalStatement);
+}
+
+function then() {
+    finalStatement.innerText = "But until then...";
+    aboutMeSection.appendChild(finalStatement);
+}
+
+function redFlash(finalStatemen) {
+finalStatement.style.color = "red;
+}
